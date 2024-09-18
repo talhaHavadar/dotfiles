4d27714dfcf98f2c62c51c396d8041a6bc03bddd
Talha Can Havadar <havadartalha@gmail.com>
2024-09-19 01:16:08 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,50 @@ sudo apt install fzf sbuild ubuntu-dev-tools apt-cacher-ng piuparts autopkgtest 
 
 sudo adduser $USER sbuild
 
+# configure tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp .tmux.conf ~/
+
+# configure git
+cp .gitconfig ~/
+mkdir -p ~/workspace
+mkdir -p ~/projects
+cp .gitconfig.workspace ~/workspace/.gitconfig
+
+# configure nvim
+git clone git@github.com:talhaHavadar/nvim-config.git nvim
+mv ~/.config/nvim ~/.config/nvim.bak
+ln -s ~/.config/dotfiles/nvim ~/.config/nvim
+
+# configure packaging tools
+cp .devscripts ~/
+cp .gbp.conf ~/
+cp .mk-sbuild.rc ~/
+cp .quiltrc-dpkg ~/
+cp .sbuildrc ~/
+
+mkdir -p $HOME/sbuild/build
+mkdir -p $HOME/sbuild/log
+mkdir -p $HOME/sbuild/scratch
+
+sudo tee -a /etc/schroot/sbuild/fstab <<EOF
+$HOME/sbuild/scratch  /scratch          none  rw,bind  0  0
+EOF
+
+sg sbuild
 
 sudo tee -a /etc/fstab <<EOF
 tmpfs		/var/lib/schroot/union/overlay/		tmpfs	defaults	0	0
 EOF
+
+
+
+
+tee -a ~/.bashrc <<EOF
+
+. ~/.config/dotfiles/.bashrc
+
+EOF
+
+setup-packaging-environment
+
