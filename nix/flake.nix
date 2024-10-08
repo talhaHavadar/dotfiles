b80952b6c859160fd0fa5985f605682ba9686f5b
Talha Can Havadar <havadartalha@gmail.com>
2024-10-08 15:17:55 +0000

    create nix directory
    Signed-off-by: Talha Can Havadar <havadartalha@gmail.com>

---
+++ nix/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "NixOS System Configuration Flake from tchavadar";
+
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }@inputs: {
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system}.default = pkgs.hello;
+      
+    };
+  };
+}
