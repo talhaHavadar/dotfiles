a6f3b5aa4b5018404b2e404bc2cb807cff7f7ebf
Talha Can Havadar <havadartalha@gmail.com>
2024-10-10 17:08:42 +0000

    tmux configuration
    Signed-off-by: Talha Can Havadar <havadartalha@gmail.com>

--- nix/tmux.nix
+++ nix/tmux.nix
@@ -1,21 +1,42 @@
 { pkgs, ... }:
 let
-in {
+  tmuxPlugins = pkgs.tmuxPlugins;
+in
+{
   programs.tmux = {
     enable = true;
     historyLimit = 100000;
     keyMode = "vi";
-    plugins = with pkgs;
-      [
-        {
-          plugin = tmuxPlugins.catppuccin;
-          extraConfig = ''
-set -g @catppuccin_flavor 'latte'
-          '';
-        }
-      ];
+    plugins = with tmuxPlugins; [
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavor 'latte'
+          set -g @catppuccin_window_status_style "rounded"
+          set -g @catppuccin_status_modules_right "load cpu user"
+        '';
+      }
+      cpu
+      sensible
+
+    ];
     extraConfig = ''
-set -g prefix C-a
+      unbind C-b
+      set -g prefix C-g
+      set -g mouse on
+
+      set-option -g detach-on-destroy off
+
+      unbind C-g
+      bind C-g select-pane -t :.+
+
+      bind h split-window -v
+      bind v split-window -h
+
+      setw -g mode-keys vi
+
+      unbind j
+      bind j run-shell -b "~/.config/dotfiles/tmux.sh"
     '';
   };
 }
