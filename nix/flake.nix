c2760abfb9af9cedf2b855a6dd81ce156eb85c63
Talha Can Havadar <havadartalha@gmail.com>
2024-10-10 07:27:25 +0000

    modularize neovim configuration
    Signed-off-by: Talha Can Havadar <havadartalha@gmail.com>

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,14 +2,17 @@
   description = "NixOS System Configuration Flake from tchavadar";
 
   nixConfig = {
-    experimental-features = [ "nix-command" "flakes" ];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
   };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
@@ -17,49 +20,66 @@
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, darwin, nixvim, ... }@inputs:
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      darwin,
+      nixvim,
+      ...
+    }@inputs:
     let
-      mkHomeConfiguration = {system, username, ... }@args: home-manager.lib.homeManagerConfiguration( rec {
-	modules = [
-	  {
-        home = {
-              inherit username;
-	      homeDirectory = "/home/${username}";
-	      stateVersion = "24.05";
-	    };
-	  }
-	  ./home.nix
-	];
-	pkgs = nixpkgs.legacyPackages.${system}; 
-	extraSpecialArgs = {
-	};
-      } // { inherit (args) extraSpecialArgs; });
+      mkHomeConfiguration =
+        { system, username, ... }@args:
+        home-manager.lib.homeManagerConfiguration (
+          rec {
+            modules = [
+              {
+                home = {
+                  inherit username;
+                  homeDirectory = "/home/${username}";
+                  stateVersion = "24.05";
+                };
+              }
+              ./home.nix
+              (import ./neovim)
+            ];
+            pkgs = nixpkgs.legacyPackages.${system};
+            extraSpecialArgs =
+              {
+              };
+          }
+          // {
+            inherit (args) extraSpecialArgs;
+          }
+        );
       username = "ubuntu";
-    in {
+    in
+    {
 
       darwinConfigurations = {
-      	hostname = darwin.lib.darwinSystem {
-	  system = "aarch64-darwin";
-	  modules = [
+        hostname = darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          modules = [
             ./system.nix
-	    home-manager.darwinModules.home-manager
-	    {
+            home-manager.darwinModules.home-manager
+            {
               home-manager.useGlobalPkgs = true;
-	      home-manager.useUserPkgs = true;
-	      home-manager.users.${username} = import ./home.nix;
-	      home-manager.backupFileExtension = "backup";
-	    }
-	    ./configs.nix
-	  ];
-	};
+              home-manager.useUserPkgs = true;
+              home-manager.users.${username} = import ./home.nix;
+              home-manager.backupFileExtension = "backup";
+            }
+            ./configs.nix
+          ];
+        };
       };
 
       homeConfigurations.${username} = mkHomeConfiguration {
         system = "x86_64-linux";
-	inherit username;
-	extraSpecialArgs = {
-	  inherit inputs;
-	};
+        inherit username;
+        extraSpecialArgs = {
+          inherit inputs;
+        };
       };
 
     };
