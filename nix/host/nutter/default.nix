84efb729b6cf7dda747b543abe33b39b5ad727f9
Talha Can Havadar <havadartalha@gmail.com>
2024-11-27 08:44:01 +0100

    nutter: add apcupsd service

--- nix/host/nutter/default.nix
+++ nix/host/nutter/default.nix
@@ -29,11 +29,19 @@
     home = "/var/lib/nut";
     hashedPassword = "$y$j9T$17hd7uG6Fy5t6eKlg76uz/$zPmSH6YLQjcYC28ebpyZQlLf8uXF6xBlBQdBbkkN9Y6";
   };
+  #services.udev.extraRules = ''
+  #  SUBSYSTEM=="usb", ATTRS{idVendor}=="0463", ATTRS{idProduct}=="ffff", MODE="664", GROUP="nut", OWNER="watcher"
+  #'';
 
-  services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="1cb0", ATTRS{idProduct}=="0038", MODE="664", GROUP="nut", OWNER="watcher"
-  '';
-
+  # ups.status OL for online OB for on battery
+  environment.etc = {
+    "nut/something.dev" = {
+      source = /home/talha/eaton_something.dev;
+      mode = "0777";
+      group = "users";
+      user = "watcher";
+    };
+  };
   power.ups = {
     enable = true;
     mode = "netserver";
@@ -45,16 +53,26 @@
       };
     };
     ups = {
-      main = {
-        driver = "usbhid-ups";
-        port = "auto";
-        description = "Main UPS";
+      #main = {
+      #  driver = "usbhid-ups";
+      #  port = "";
+      #  description = "Main UPS";
+      #};
+      dummy = {
+        driver = "dummy-ups";
+        port = "something.dev";
+        description = "dummy-ups dummy-once mode";
       };
     };
     upsmon = {
       enable = true;
       monitor = {
-        main = {
+        #main = {
+        #  powerValue = 1;
+        #  user = "watcher";
+        #  type = "primary";
+        #};
+        dummy = {
           powerValue = 1;
           user = "watcher";
           type = "primary";
@@ -72,7 +90,39 @@
           address = "0.0.0.0";
           port = 3493;
         }
+        {
+          address = "127.0.0.1";
+          port = 3493;
+        }
       ];
+    };
+  };
+  services.apcupsd = {
+    enable = true;
+    configText = ''
+        UPSTYPE usb
+        DEVICE
+        BATTERYLEVEL 5
+        MINUTES 5
+        ONBATTERYDELAY 30
+        NISIP 127.0.0.1
+        NISPORT 3551
+    '';
+    hooks = {
+      onbattery = ''
+        echo "UPS is on battery!!"
+        now="$(date -Iseconds)"
+        echo "$now" >> /home/talha/upsbattery.log
+	sed -i "s/ups.status:.*/ups.status: OB/" /etc/nut/something.dev
+        exit 99
+      '';
+      mainsback = ''
+        echo "UPS is on main power!!"
+        now="$(date -Iseconds)"
+        echo "$now" >> /home/talha/upsmains.log
+	sed -i "s/ups.status:.*/ups.status: OL/" /etc/nut/something.dev
+        exit 99
+      '';
     };
   };
   # List packages installed in system profile. To search, run:
