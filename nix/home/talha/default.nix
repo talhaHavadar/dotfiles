913b67b4e345e31c5b288eaaa290dd8d064d7d9e
Talha Can Havadar <havadartalha@gmail.com>
2025-04-20 15:53:23 +0000

    nixos: talha: add vpn configuration

--- nix/home/talha/default.nix
+++ nix/home/talha/default.nix
@@ -371,5 +371,17 @@ with lib;
       };
     }
     // optionalAttrs (currentConfigSystem == "nixos") {
+      services.openvpn.servers = {
+        tw-vpn = {
+          config = ''config /etc/nixos/talha-vpn/tw-tchavadar.conf '';
+          autoStart = false;
+          #updateResolvConf = true;
+        };
+        uk-vpn = {
+          config = ''config /etc/nixos/talha-vpn/uk-tchavadar.conf '';
+          autoStart = false;
+          #updateResolvConf = true;
+        };
+      };
     };
 }
