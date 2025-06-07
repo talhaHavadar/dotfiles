9de8d8d9d930efe0fb76d893d42872206e1403d8
Talha Can Havadar <havadartalha@gmail.com>
2025-06-07 09:52:53 +0200

    ghostty: add xdg desktop entry and customize the settings

--- nix/ghostty.nix
+++ nix/ghostty.nix
@@ -28,16 +28,38 @@ with lib;
       enable = true;
       # enableBashIntegration = true;
       settings = {
-        theme = "catppuccin-mocha";
+        theme = "catppuccin-frappe";
         #font-size = 10;
+        "gtk-titlebar" = false;
+        "cursor-style" = "block";
+        "window-colorspace" = "display-p3";
+        "background-opacity" = 0.9;
+        "background-blur" = 5;
+        "unfocused-split-opacity" = 0.9;
+        "macos-option-as-alt" = "left";
         keybind = [
-          "super+right=next_tab"
-          "super+left=previous_tab"
-          "super+shift+w=close_tab"
-          "super+shift+t=new_tab"
-          "super+shift+enter=new_split:down"
-          "super+shift+plus=increase_font_size:1"
-          "super+shift+minus=decrease_font_size:1"
+          # Keybinds to match macOS since this is a VM
+          "super+c=copy_to_clipboard"
+          "super+v=paste_from_clipboard"
+          "super+shift+c=copy_to_clipboard"
+          "super+shift+v=paste_from_clipboard"
+          "super+plus=increase_font_size:1"
+          "super+minus=decrease_font_size:1"
+          "super+zero=reset_font_size"
+          "super+q=quit"
+          "super+shift+comma=reload_config"
+          "super+n=new_window"
+          "super+w=close_surface"
+          "super+shift+w=close_window"
+          "super+t=new_tab"
+          "super+shift+left=previous_tab"
+          "super+shift+right=next_tab"
+          "super+ctrl+v=new_split:right"
+          "super+ctrl+h=new_split:down"
+          "super+shift+j=goto_split:down"
+          "super+shift+k=goto_split:up"
+          "super+shift+h=goto_split:left"
+          "super+shift+l=goto_split:right"
         ];
       };
       # themes = {
@@ -67,6 +89,23 @@ with lib;
       #     selection-foreground = "cdd6f4";
       #   };
       # };
+    };
+
+    home.shellAliases.ghostty = "${pkgs.ghostty}/bin/ghostty";
+    xdg.dataFile."applications/ghostty.desktop" = {
+      # TODO: not needed/effective in macos
+      text = ''
+        [Desktop Entry]
+        Version=1.0
+        Type=Application
+        Name=ghostty
+        GenericName=Terminal emulator
+        Comment=Fast, feature-rich, GPU based terminal
+        TryExec=${pkgs.ghostty}/bin/ghostty
+        Exec=${pkgs.ghostty}/bin/ghostty
+        Icon=${pkgs.ghostty}/share/icons/hicolor/256x256/apps/com.mitchellh.ghostty.png
+        Categories=System;TerminalEmulator;
+      '';
     };
   };
 
