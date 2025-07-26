f809c619e04ad21821a02012e4e189827521b232
Talha Can Havadar <havadartalha@gmail.com>
2025-07-26 19:26:17 +0200

    nix: update flakes

--- nix/terminal.nix
+++ nix/terminal.nix
@@ -31,56 +31,53 @@ with lib;
 
     bash = {
       enable = true;
-      bashrcExtra =
-        ''
-          tmux_find_or_create_prompt() {
-              query="$(zoxide query -l)"$'\n'"$(tmux list-sessions)"
-              result=$(echo "$query" | fzf-tmux -p -w 62% -h 38% -m)
-              if [ "$result" = "" ]; then
-                  echo ""
-              else
-                  zoxide add "$result" &>/dev/null
-                  session_name=$(echo $result | sed "s/:.*//g" | sed "s/.*\///g")
-                  if [ "$TMUX" ]; then
-                      tmux switch-client -t $session_name || (cd $result && tmux new-session -d -s $session_name && cd - && tmux switch-client -t $session_name)
-                  else
-                      cd $result
-                      tmux new -As $session_name
-                  fi
-              fi
-          }
+      bashrcExtra = ''
+        tmux_find_or_create_prompt() {
+            query="$(zoxide query -l)"$'\n'"$(tmux list-sessions)"
+            result=$(echo "$query" | fzf-tmux -p -w 62% -h 38% -m)
+            if [ "$result" = "" ]; then
+                echo ""
+            else
+                zoxide add "$result" &>/dev/null
+                session_name=$(echo $result | sed "s/:.*//g" | sed "s/.*\///g")
+                if [ "$TMUX" ]; then
+                    tmux switch-client -t $session_name || (cd $result && tmux new-session -d -s $session_name && cd - && tmux switch-client -t $session_name)
+                else
+                    cd $result
+                    tmux new -As $session_name
+                fi
+            fi
+        }
 
-          lxc-update-ssh-keys() {
-            container_name=$1
-            cat ~/.ssh/id_ed25519.pub | lxc exec "$1" -- \
-            sh -c "cat >> /home/ubuntu/.ssh/authorized_keys"
-            lxc exec "$1" -- systemctl restart ssh
-          }
+        lxc-update-ssh-keys() {
+          container_name=$1
+          cat ~/.ssh/id_ed25519.pub | lxc exec "$1" -- \
+          sh -c "cat >> /home/ubuntu/.ssh/authorized_keys"
+          lxc exec "$1" -- systemctl restart ssh
+        }
 
-
-
-        ''
-        + optionalString (platform == "ubuntu-headless") ''
-          update-home() {
-            NIXPKGS_ALLOW_UNFREE=1 NIX_MYUSER="$USER" home-manager switch --flake ~/.config/dotfiles/nix#ubuntu-headless --show-trace --impure -b backup
-          }
-        ''
-        + optionalString (platform == "non-nixos") ''
-          update-home() {
-            NIXPKGS_ALLOW_UNFREE=1 NIX_MYUSER="$USER" home-manager switch --flake ~/.config/dotfiles/nix#linux --show-trace --impure -b backup
-          }
-        ''
-        + optionalString (platform == "macos") ''
-          update-home() {
-            NIXPKGS_ALLOW_UNFREE=1 NIX_MYUSER="$USER" darwin-rebuild switch --flake ~/.config/dotfiles/nix#macpro --show-trace --impure
-          }
-        ''
-        + optionalString (platform == "nixos") ''
-          update-system() {
-            NIXPKGS_ALLOW_UNFREE=1 NIX_MYUSER="$USER" sudo -E nixos-rebuild switch --flake ~/.config/dotfiles/nix#"$HOSTNAME" --show-trace --impure
-          }
-        ''
-        + optionalString isPackagingEnabled ". ~/.packaging.bashrc";
+      ''
+      + optionalString (platform == "ubuntu-headless") ''
+        update-home() {
+          NIXPKGS_ALLOW_UNFREE=1 NIX_MYUSER="$USER" home-manager switch --flake ~/.config/dotfiles/nix#ubuntu-headless --show-trace --impure -b backup
+        }
+      ''
+      + optionalString (platform == "non-nixos") ''
+        update-home() {
+          NIXPKGS_ALLOW_UNFREE=1 NIX_MYUSER="$USER" home-manager switch --flake ~/.config/dotfiles/nix#linux --show-trace --impure -b backup
+        }
+      ''
+      + optionalString (platform == "macos") ''
+        update-home() {
+          NIXPKGS_ALLOW_UNFREE=1 NIX_MYUSER="$USER" sudo -E darwin-rebuild switch --flake ~/.config/dotfiles/nix#mac --show-trace --impure
+        }
+      ''
+      + optionalString (platform == "nixos") ''
+        update-system() {
+          NIXPKGS_ALLOW_UNFREE=1 NIX_MYUSER="$USER" sudo -E nixos-rebuild switch --flake ~/.config/dotfiles/nix#"$HOSTNAME" --show-trace --impure
+        }
+      ''
+      + optionalString isPackagingEnabled ". ~/.packaging.bashrc";
       initExtra =
         ''''
         + optionalString (platform == "macos") ''
@@ -104,22 +101,21 @@ with lib;
           source ~/.extra_bashrc 2>/dev/null || true
         '';
 
-      shellAliases =
-        {
-          gg = "git grep --no-index";
-          ls = "ls --color=auto";
-          ll = "ls --color=auto -al";
-          tn = "tmux new -As $(pwd | sed \"s/.*\\///g\")";
-          t = "tmux new -A -s";
-          tl = "tmux list-sessions";
-          tk = "tmux kill-session -t";
-          tf = "tmux_find_or_create_prompt";
-          tp = "tmux list-panes -a -F '#D #T #{pane_tty} #{pane_current_command} #{pane_current_path}'";
-          gvim = "nvim --listen ~/.cache/nvim/godot.pipe .";
-        }
-        // optionalAttrs isPackagingEnabled {
-          update-home = "INCLUDE_PACKAGING=\"true\" update-home";
-        };
+      shellAliases = {
+        gg = "git grep --no-index";
+        ls = "ls --color=auto";
+        ll = "ls --color=auto -al";
+        tn = "tmux new -As $(pwd | sed \"s/.*\\///g\")";
+        t = "tmux new -A -s";
+        tl = "tmux list-sessions";
+        tk = "tmux kill-session -t";
+        tf = "tmux_find_or_create_prompt";
+        tp = "tmux list-panes -a -F '#D #T #{pane_tty} #{pane_current_command} #{pane_current_path}'";
+        gvim = "nvim --listen ~/.cache/nvim/godot.pipe .";
+      }
+      // optionalAttrs isPackagingEnabled {
+        update-home = "INCLUDE_PACKAGING=\"true\" update-home";
+      };
     };
   };
 
