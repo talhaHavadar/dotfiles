9cbc370eae24ad9969150c465bc030b98e87e22a
Talha Can Havadar <havadartalha@gmail.com>
2026-02-19 19:36:03 +0100

    nvim: add gitsigns

--- dot/nvim/lua/plugins/git.lua
+++ dot/nvim/lua/plugins/git.lua
@@ -1,7 +1,13 @@
 return {
-  {
-    "sindrets/diffview.nvim",
-    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
-    config = true,
-  },
+    {
+        "sindrets/diffview.nvim",
+        cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+        config = true,
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+        end,
+    },
 }
