0244f3299faca96c51a32d5c0998063880d87b92
Talha Can Havadar <havadartalha@gmail.com>
2026-02-19 19:50:53 +0100

    nvim: add keymaps for gitsigns

--- dot/nvim/lua/plugins/git.lua
+++ dot/nvim/lua/plugins/git.lua
@@ -1,13 +1,18 @@
 return {
-    {
-        "sindrets/diffview.nvim",
-        cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
-        config = true,
-    },
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup()
-        end,
-    },
+	{
+		"sindrets/diffview.nvim",
+		cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+		config = true,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				current_line_blame = true,
+			})
+			local keymap = vim.keymap.set
+
+			keymap("n", "<leader>gh", "<cmd>Gitsigns preview_hunk_inline<CR>")
+		end,
+	},
 }
